8d6f9856d4e28635e69acc0659487cb6283570ce
Amanda Nielsen <amandasn202@gmail.com>
2025-11-27 15:22:08 +0100

    footer styling and function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LandingPage } from '@nattugglan/landingpage';
+//import { Footer } from '@nattugglan/footer'; > lägg till på de sidor som ska ha footer
 
 
 function App() {
