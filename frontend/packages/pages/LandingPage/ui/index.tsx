7030e8db102101f505735abc1ebe7b903f5c8f38
Amanda Nielsen <amandasn202@gmail.com>
2025-11-27 12:29:52 +0100

    landingpage and router

---
+++ frontend/packages/pages/LandingPage/ui/index.tsx
@@ -0,0 +1,16 @@
+import './index.css';
+
+function LandingPage() {
+  return (
+    <section className="landingpage">
+      
+      <div className="landing__content">
+        <h1>Vaken? Vi med!</h1>
+				<img className="logo__landing" src="./assets/logo.png" alt="nattugglan logo" />
+      </div>
+
+    </section>
+  )
+}
+
+export { LandingPage }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { LandingPage } from '@nattugglan/landingpage';
+
+
 function App() {
 	return (
-		<>
-			<img className="forrest" src="./src/assets/BG-forrest.png" />
-		</>
+		<BrowserRouter>
+			<section>
+				<img className="forrest" src="./src/assets/BG-forrest-phone.png" />
+				<Routes>
+					<Route path="/" element={<LandingPage />} />
+				</Routes>
+			</section>
+    </BrowserRouter>
 	);
 }
 
