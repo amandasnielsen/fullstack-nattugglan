b36ec01786e02aaff1e2faa61867b06663d3bc8a
Amanda Nielsen <amandasn202@gmail.com>
2025-12-02 14:41:45 +0100

    new forrest image

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,7 +17,8 @@ function App() {
 	return (
 		<BrowserRouter>
 			<section>
-				<img className="forrest" src="./src/assets/BG-forrest-phone.png" />
+				{/*<img className="forrest" src="./src/assets/BG-forrest-phone.png" />*/}
+				<img className="forrest" src="./src/assets/BG-forrest-new.jpg" />
 				<Routes>
 					<Route path="/" element={<LandingPage />} />
 					<Route path="/menu" element={<MenuPage />} />
