d08f59ed483d1f30a67bbad89cf92d44afc5357c
Amanda Nielsen <amandasn202@gmail.com>
2025-12-03 15:09:57 +0100

    links for desktop

--- frontend/packages/base/Navbar/ui/index.tsx
+++ frontend/packages/base/Navbar/ui/index.tsx
@@ -20,150 +20,235 @@ function NavBar() {
 	//Lägg även till funktion för att kunna logga ut när de finns användare
 
 	return (
-		<header className="navbar__container">
-			<div>
-				<NavLink to="/menu">
-					<img src={Logo} className="navbar__logo" alt="Företagslogotyp" />
-				</NavLink>
-			</div>
+		<section className="navbar">
+			<header className="navbar__container-phone">
+				<div>
+					<NavLink to="/menu">
+						<img src={Logo} className="navbar__logo" alt="Företagslogotyp" />
+					</NavLink>
+				</div>
 
-      <div className="navbar__icons-right">
-        
-        <div className="navbar__icons-right">
-        
-					{totalQuantity > 0 && (
-						<NavLink 
-							to="/cart"
-							className="navbar__cart-link"
+				<div className="navbar__icons-right">
+					
+					<div className="navbar__icons-right">
+					
+						{totalQuantity > 0 && (
+							<NavLink 
+								to="/cart"
+								className="navbar__cart-link"
+							>
+								<img 
+									src={CartIcon} 
+									className="navbar__cart-icon" 
+									alt="Kundkorg" 
+								/>
+
+								<span className="navbar__cart-badge">
+									{totalQuantity}
+								</span>
+							</NavLink>
+						)}
+
+						<div
+							className={`navbar__menuIcon ${menuOpen ? 'open' : ''}`}
+							onClick={toggleMenu}
+							aria-label={menuOpen ? 'Stäng Meny' : 'Öppna Meny'}
+							role="button"
 						>
-							<img 
-								src={CartIcon} 
-								className="navbar__cart-icon" 
-								alt="Kundkorg" 
-							/>
-
-							<span className="navbar__cart-badge">
-								{totalQuantity}
-							</span>
-						</NavLink>
-					)}
-
-					<div
-						className={`navbar__menuIcon ${menuOpen ? 'open' : ''}`}
-						onClick={toggleMenu}
-						aria-label={menuOpen ? 'Stäng Meny' : 'Öppna Meny'}
-						role="button"
-					>
-						<div className="bar bar1"></div>
-						<div className="bar bar2"></div>
-						<div className="bar bar3"></div>
+							<div className="bar bar1"></div>
+							<div className="bar bar2"></div>
+							<div className="bar bar3"></div>
+						</div>
 					</div>
 				</div>
-      </div>
 
-			<nav className={`navbar__menu ${menuOpen ? 'visible' : ''}`}>
-				<div className="navbar__menu-top">
+				<nav className={`navbar__menu ${menuOpen ? 'visible' : ''}`}>
+					<div className="navbar__menu-top">
+						<NavLink
+							className={({ isActive }) =>
+								isActive ? 'navbar__menuLinks active-link' : 'navbar__menuLinks'
+							}
+							to="/menu"
+							onClick={toggleMenu}
+						>
+							Meny
+						</NavLink>
+						<NavLink
+							className={({ isActive }) =>
+								isActive ? 'navbar__menuLinks active-link' : 'navbar__menuLinks'
+							}
+							to="/orderstatus"
+							onClick={toggleMenu}
+						>
+							Orderstatus
+						</NavLink>
+						<NavLink
+							className={({ isActive }) =>
+								isActive ? 'navbar__menuLinks active-link' : 'navbar__menuLinks'
+							}
+							to="/myorders"
+							onClick={toggleMenu}
+						>
+							Tidigare Beställningar
+						</NavLink>
+						<NavLink
+							className={({ isActive }) =>
+								isActive ? 'navbar__menuLinks active-link' : 'navbar__menuLinks'
+							}
+							to="/aboutUs"
+							onClick={toggleMenu}
+						>
+							Om oss
+						</NavLink>
+						<NavLink
+							className={({ isActive }) =>
+								isActive ? 'navbar__menuLinks active-link' : 'navbar__menuLinks'
+							}
+							to="/maps"
+							onClick={toggleMenu}
+						>
+							Vart finns vi?
+						</NavLink>
+					</div>
 					<NavLink
 						className={({ isActive }) =>
 							isActive ? 'navbar__menuLinks active-link' : 'navbar__menuLinks'
 						}
-						to="/menu"
+						to="/login"
 						onClick={toggleMenu}
 					>
-						Meny
+						Logga in som Admin
 					</NavLink>
-					<NavLink
-						className={({ isActive }) =>
-							isActive ? 'navbar__menuLinks active-link' : 'navbar__menuLinks'
-						}
-						to="/orderstatus"
-						onClick={toggleMenu}
-					>
-						Orderstatus
-					</NavLink>
-					<NavLink
-						className={({ isActive }) =>
-							isActive ? 'navbar__menuLinks active-link' : 'navbar__menuLinks'
-						}
-						to="/myorders"
-						onClick={toggleMenu}
-					>
-						Tidigare Beställningar
-					</NavLink>
-					<NavLink
-						className={({ isActive }) =>
-							isActive ? 'navbar__menuLinks active-link' : 'navbar__menuLinks'
-						}
-						to="/aboutUs"
-						onClick={toggleMenu}
-					>
-						Om oss
-					</NavLink>
-					<NavLink
-						className={({ isActive }) =>
-							isActive ? 'navbar__menuLinks active-link' : 'navbar__menuLinks'
-						}
-						to="/maps"
-						onClick={toggleMenu}
-					>
-						Vart finns vi?
-					</NavLink>
-				</div>
-				<NavLink
-					className={({ isActive }) =>
-						isActive ? 'navbar__menuLinks active-link' : 'navbar__menuLinks'
-					}
-					to="/login"
-					onClick={toggleMenu}
-				>
-					Logga in som Admin
-				</NavLink>
-			</nav>
+				</nav>
 
-			{/* länkar till när man kan logga in som admin
-			
-			alla beställningar
-			uppdatera menyn
-			lagerstatus
+				{/* länkar till när man kan logga in som admin
+				
+				alla beställningar
+				uppdatera menyn
+				lagerstatus
 
-			logga ut
-			
-			<nav className={`navbar__menu ${menuOpen ? 'visible' : ''}`}>
-				<div className="navbar__menu-top">
-					<NavLink
+				logga ut
+				
+				<nav className={`navbar__menu ${menuOpen ? 'visible' : ''}`}>
+					<div className="navbar__menu-top">
+						<NavLink
+							className={({ isActive }) =>
+								isActive ? 'navbar__menuLinks active-link' : 'navbar__menuLinks'
+							}
+							to="/allorders"
+							onClick={toggleMenu}
+						>
+							Alla Beställningar
+						</NavLink>
+						<NavLink
+							className={({ isActive }) =>
+								isActive ? 'navbar__menuLinks active-link' : 'navbar__menuLinks'
+							}
+							to="/updatemenu"
+							onClick={toggleMenu}
+						>
+							Uppdatera Menyn
+						</NavLink>
+						<NavLink
+							className={({ isActive }) =>
+								isActive ? 'navbar__menuLinks active-link' : 'navbar__menuLinks'
+							}
+							to="/stock"
+							onClick={toggleMenu}
+						>
+							Lagerstatus
+						</NavLink>
+					</div>
+				<button className="navbar__logout">Logga ut</button>
+				</nav>
+				*/}
+
+			</header>
+
+			<header className="navbar__container-desktop">
+				<section className="navbar__container-span">
+					<div>
+						<NavLink to="/menu">
+							<img src={Logo} className="navbar__logo" alt="Företagslogotyp" />
+						</NavLink>
+					</div>
+					<div className="navbar__desktop-group">
+						<NavLink
+							className={({ isActive }) =>
+								isActive ? 'navbar__desktop-links active-link-desktop' : 'navbar__desktop-links'
+							}
+							to="/menu"
+							onClick={toggleMenu}
+						>
+							Meny
+						</NavLink>
+						<NavLink
+							className={({ isActive }) =>
+								isActive ? 'navbar__desktop-links active-link-desktop' : 'navbar__desktop-links'
+							}
+							to="/orderstatus"
+							onClick={toggleMenu}
+						>
+							Orderstatus
+						</NavLink>
+						<NavLink
+							className={({ isActive }) =>
+								isActive ? 'navbar__desktop-links active-link-desktop' : 'navbar__desktop-links'
+							}
+							to="/myorders"
+							onClick={toggleMenu}
+						>
+							Beställningar
+						</NavLink>
+						<NavLink
+							className={({ isActive }) =>
+								isActive ? 'navbar__desktop-links active-link-desktop' : 'navbar__desktop-links'
+							}
+							to="/aboutUs"
+							onClick={toggleMenu}
+						>
+							Om oss
+						</NavLink>
+						<NavLink
+							className={({ isActive }) =>
+								isActive ? 'navbar__desktop-links active-link-desktop' : 'navbar__desktop-links'
+							}
+							to="/maps"
+							onClick={toggleMenu}
+						>
+							Vart finns vi?
+						</NavLink>
+						<NavLink
 						className={({ isActive }) =>
-							isActive ? 'navbar__menuLinks active-link' : 'navbar__menuLinks'
+							isActive ? 'navbar__desktop-links active-link-desktop' : 'navbar__desktop-links'
 						}
-						to="/allorders"
+						to="/login"
 						onClick={toggleMenu}
 					>
-						Alla Beställningar
+						Admin
 					</NavLink>
-					<NavLink
-						className={({ isActive }) =>
-							isActive ? 'navbar__menuLinks active-link' : 'navbar__menuLinks'
-						}
-						to="/updatemenu"
-						onClick={toggleMenu}
-					>
-						Uppdatera Menyn
-					</NavLink>
-					<NavLink
-						className={({ isActive }) =>
-							isActive ? 'navbar__menuLinks active-link' : 'navbar__menuLinks'
-						}
-						to="/stock"
-						onClick={toggleMenu}
-					>
-						Lagerstatus
-					</NavLink>
-				</div>
-			<button className="navbar__logout">Logga ut</button>
-			</nav>
-			*/}
+					</div>
+				</section>
+			</header>
+			{/*<div className="navbar__desktop-cart">			
+						{totalQuantity > 0 && (
+							<NavLink 
+								to="/cart"
+								className="navbar__cart-link"
+							>
+								<img 
+									src={CartIcon} 
+									className="navbar__cart-icon" 
+									alt="Kundkorg" 
+								/>
 
-
-		</header>
+								<span className="navbar__cart-badge">
+									{totalQuantity}
+								</span>
+							</NavLink>
+						)}
+					</div>*/}
+		</section>
 	);
 }
 
