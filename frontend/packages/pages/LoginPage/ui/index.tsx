65e64024033d9950afde54fbefc19dac213a62eb
Rasmus Frolov <rasmusfrolov@live.se>
2025-12-04 11:55:58 +0100

    admin log in frontend

--- frontend/packages/pages/LoginPage/ui/index.tsx
+++ frontend/packages/pages/LoginPage/ui/index.tsx
@@ -2,8 +2,44 @@ import "./index.css";
 import { NavBar } from "@nattugglan/navbar";
 import { Footer } from "@nattugglan/footer";
 import { Button } from "@nattugglan/button";
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
 function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  async function handleLogin() {
+    try {
+      const res = await fetch("http://localhost:3000/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": "yourapikey"
+        },
+        body: JSON.stringify({ username, password })
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        alert(data.error || "Login failed");
+        return;
+      }
+
+      localStorage.setItem("adminToken", data.token);
+
+      alert("Logged in!");
+
+      navigate("/updatemenu");
+
+    } catch (err) {
+      console.error(err);
+      alert("Something went wrong");
+    }
+  }
+
   return (
     <>
       <NavBar />
@@ -20,6 +56,8 @@ function LoginPage() {
               id="username"
               type="text"
               className="login__input"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
             />
 
             <label className="login__label" htmlFor="password">
@@ -29,12 +67,15 @@ function LoginPage() {
               id="password"
               type="password"
               className="login__input"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
 
             <Button
               variant="primary"
               fullWidth={true}
               className="landing__button-login login__button"
+              onClick={handleLogin}
             >
               Logga in
             </Button>
