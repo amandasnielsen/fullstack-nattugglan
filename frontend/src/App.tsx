10f50542fa0cbf5519b813b8fafaf1fe3147b535
Amanda Nielsen <amandasn202@gmail.com>
2025-12-15 10:20:30 +0100

    moved router from App.tsx to own file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,58 +1,20 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { LandingPage } from '@nattugglan/landingpage';
-import { MenuPage } from '@nattugglan/menupage';
-import { OrderStatusPage } from '@nattugglan/orderstatuspage';
-import { MyOrdersPage } from '@nattugglan/myorderspage';
-import { AboutUs } from '@nattugglan/aboutus';
-import { MapPage } from '@nattugglan/mappage';
-import { LoginPage } from '@nattugglan/loginpage';
-import { AdminChangeMenuPage } from '@nattugglan/adminchangemenupage';
-import { StockPage } from '@nattugglan/stockpage';
-import { AdminAllOrdersPage } from '@nattugglan/adminallorderspage';
-import { PaymentPage } from '@nattugglan/paymentpage';
-import { OrderConfirmationPage } from '@nattugglan/orderconfirmationpage';
-import { CartPage } from '@nattugglan/cartpage';
+import { BrowserRouter } from 'react-router-dom';
+import { AppRouter } from './routes/AppRouter'; 
 import ForrestBackground from './assets/BG-forrest.jpg';
-import { ProtectedRoute } from '@nattugglan/protectedroute';
-import { AccessDenied } from '@nattugglan/accessdenied';
 import { useGlobalOrderStatusPolling } from '@nattugglan/core';
 
 function App() {
 
-	useGlobalOrderStatusPolling();
-	
+  useGlobalOrderStatusPolling();
+  
   return (
     <BrowserRouter>
-      <section>
+      <div className="app">
+        
         <img className="forrest" src={ForrestBackground} alt="Skogsbakgrund" />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/menu" element={<MenuPage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/payment" element={<PaymentPage />} />
-          <Route path="/orderstatus/:orderNumber" element={<OrderStatusPage />} />
-          <Route path="/myorders" element={<MyOrdersPage />} />
-          <Route path="/aboutUs" element={<AboutUs />} />
-          <Route path="/maps" element={<MapPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/order/:orderNumber" element={<OrderConfirmationPage />} />
-
-          {/* --- Protected Admin-Routes --- */}
-          <Route 
-            path="/allorders" 
-            element={<ProtectedRoute component={AdminAllOrdersPage} requiredRole="admin" />} 
-          />
-          <Route 
-            path="/updatemenu" 
-            element={<ProtectedRoute component={AdminChangeMenuPage} requiredRole="admin" />} 
-          />
-          <Route 
-            path="/stock" 
-            element={<ProtectedRoute component={StockPage} requiredRole="admin" />} 
-          />
-          <Route path="/access-denied" element={<AccessDenied />} />        
-        </Routes>
-      </section>
+        <AppRouter /> 
+        
+      </div>
     </BrowserRouter>
   );
 }
